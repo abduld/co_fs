a832dc84349a2fe2b58763b0299d98377c96e9b9
Abdul Dakkak <adakkak@gmail.com>
2022-04-02 01:09:19 -0700

    checkpoint

--- include/co_fs/co_fs.hpp
+++ include/co_fs/co_fs.hpp
@@ -2,9 +2,10 @@
 
 #include <string>
 #include <thread>
+#include <filesystem>
 
 namespace co_fs {
-inline auto open(const std::string &path) -> void * {
-  return nullptr;
+inline auto open(const std::string &path) -> std::string {
+  return path;
 }
 } // namespace co_fs
